ee6949de87f0b7de0d7044a4a47b21857ba2d55f
hnakashima <nakashima.alg57@gmail.com>
2023-06-09 11:15:35 +0900

    feat: Add hello-world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use axum::routing::get;
+use axum::routing::Router;
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() -> Result<(), hyper::Error> {
+    let app = Router::new().route("/", get(root));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    axum::Server::bind(&addr).serve(app.into_make_service()).await
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
 }
