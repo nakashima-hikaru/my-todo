a95fccbeadca14dd30b6cdd89adf64d91632f41a
hnakashima <nakashima.alg57@gmail.com>
2023-06-10 11:46:11 +0900

    test: Add a test for update-handler

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,11 +1,24 @@
+use std::sync::Arc;
 use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
-use crate::repositories::{CreateTodo, TodoRepository};
+use axum::extract::Path;
+use crate::repositories::{CreateTodo, TodoRepository, UpdateTodo};
 
 pub async fn create_todo<T: TodoRepository>(
-    State(repository): State<T>,
+    State(repository): State<Arc<T>>,
     Json(payload): Json<CreateTodo>,
 ) -> impl IntoResponse {
     let todo = repository.create(payload);
 
     (StatusCode::CREATED, Json(todo))
+}
+
+pub async fn update_todo<T: TodoRepository>(
+    Path(id): Path<i32>,
+    State(repository): State<Arc<T>>,
+    Json(payload): Json<UpdateTodo>,
+) -> Result<impl IntoResponse, StatusCode> {
+    let todo = repository
+        .update(id, payload)
+        .or(Err(StatusCode::NOT_FOUND))?;
+    Ok((StatusCode::CREATED, Json(todo)))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 mod repositories;
 mod handlers;
 
-use axum::routing::{get, post};
+use axum::routing::{get, patch, post};
 use axum::routing::Router;
 use std::net::SocketAddr;
 use std::env;
-use crate::handlers::create_todo;
+use std::sync::Arc;
+use crate::handlers::{create_todo, update_todo};
 use crate::repositories::{TodoRepository, TodoRepositoryForMemory};
 
 
@@ -15,18 +16,23 @@ async fn main() -> Result<(), hyper::Error> {
     env::set_var("RUST_LOG", log_level);
     tracing_subscriber::fmt::init();
     let repository = TodoRepositoryForMemory::new();
-    let app = create_app(repository);
+    let app = create_app(repository.into());
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::debug!("listening on {}", addr);
     tracing::debug!("http://localhost:3000");
     axum::Server::bind(&addr).serve(app.into_make_service()).await
 }
 
-fn create_app<T: TodoRepository>(repository: T) -> Router {
+fn create_app<T: TodoRepository>(repository: Arc<T>) -> Router {
     Router::new()
         .route("/", get(root))
         .route("/todos", post(create_todo::<T>))
-        .with_state(repository)
+        .with_state(Arc::clone(&repository))
+        .route(
+            "/todos/:id",
+            patch(update_todo::<T>),
+        )
+        .with_state(Arc::clone(&repository))
 }
 
 async fn root() -> &'static str {
@@ -39,18 +45,55 @@ mod test {
     use super::*;
     use axum::body::Body;
     use axum::http;
+    use axum::http::{header, Method};
+    use axum::response::Response;
     use http::{Request};
 
     use tower::ServiceExt;
+    use crate::repositories::{CreateTodo, Todo};
+
+    fn build_request_with_json(path: &str, method: Method, json_body: String) -> Request<Body> {
+        Request::builder()
+            .uri(path)
+            .method(method)
+            .header(header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+            .body(Body::from(json_body))
+            .unwrap()
+    }
+
+    async fn res_to_todo(res: Response) -> Todo {
+        let bytes = hyper::body::to_bytes(res.into_body()).await.unwrap();
+        let body: String = String::from_utf8(bytes.to_vec()).unwrap();
+        let todo: Todo = serde_json::from_str(&body)
+            .expect(&format!("cannot convert Todo instance. body: {}", body));
+        todo
+    }
 
     #[tokio::test]
     async fn should_return_hello_world() {
         let req = Request::builder().uri("/").body(Body::empty()).unwrap();
         let repository = TodoRepositoryForMemory::new();
-        let res = create_app(repository).oneshot(req).await.unwrap();
+        let res = create_app(repository.into()).oneshot(req).await.unwrap();
 
         let bytes = hyper::body::to_bytes(res.into_body()).await.unwrap();
         let body: String = String::from_utf8(bytes.to_vec()).unwrap();
         assert_eq!(body, "Hello, World!");
+    }
+
+    #[tokio::test]
+    async fn should_update_todo() {
+        let expected = Todo::new(1, "should_update_todo".to_string());
+
+        let repository = TodoRepositoryForMemory::new();
+        repository.create(CreateTodo::new("before_update_todo".to_string()));
+        let req = build_request_with_json(
+            "/todos/1",
+            Method::PATCH,
+            r#"{"text": "should_update_todo","completed": false}"#
+                .to_string(),
+        );
+        let res = create_app(repository.into()).oneshot(req).await.unwrap();
+        let todo = res_to_todo(res).await;
+        assert_eq!(expected, todo);
     }
 }
